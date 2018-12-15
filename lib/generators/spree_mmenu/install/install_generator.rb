3b6908d25b726b31316cb3e68d801524aa645d33
Matthew Kennedy <m.kennedy@me.com>
2018-12-15 19:11:06 +0000

    make better

--- lib/generators/spree_mmenu/install/install_generator.rb
+++ lib/generators/spree_mmenu/install/install_generator.rb
@@ -1,7 +1,16 @@
 module SpreeMmenu
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      
+      class_option :auto_run_migrations, type: :boolean, default: false
+
+      def add_javascripts
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_mmenu\n"
+      end
+
+      def add_stylesheets
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_mmenu\n", before: %r{\*\/}, verbose: true
+      end
+
     end
   end
 end
