3c140b5b11f31791a2fe7e1f9927c70c0beba00c
Matthew Kennedy <m.kennedy@me.com>
2018-12-15 18:44:58 +0000

    Init

---
+++ lib/generators/spree_mmenu/install/install_generator.rb
@@ -0,0 +1,7 @@
+module SpreeMmenu
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      
+    end
+  end
+end
---
+++ lib/spree_mmenu.rb
@@ -0,0 +1,4 @@
+require 'spree_core'
+require 'spree_extension'
+require 'spree_mmenu/engine'
+require 'spree_mmenu/version'
